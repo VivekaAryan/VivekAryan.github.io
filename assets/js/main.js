b21211f4bae5412860c1fd405e55c8ef672d6a4e
Vivek Aryan <aryanravula@gmail.com>
2023-12-31 13:24:32 -0500

    Header and Nav Menu Draft 1. Needs to be QAed.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,34 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelector('.nav__link')
 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__Link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
