e5df365133a3192a8def7078b8600525112c38c1
Vivek Aryan <aryanravula@gmail.com>
2024-01-03 21:57:03 -0500

    Added Skills. Need to debug the close/open.

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,7 @@ if(navClose){
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
+
 const navLink = document.querySelector('.nav__link')
 
 function linkAction(){
@@ -32,7 +33,23 @@ navLink.forEach(n => addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
+const skillsContent = document.getElementsByClassName('skills__content');
+const skillsHeader = document.querySelectorAll('.skills__header')
 
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
 /*==================== QUALIFICATION TABS ====================*/
 
 
